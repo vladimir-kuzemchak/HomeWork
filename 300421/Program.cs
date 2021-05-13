01c39ae4b2ecba743f85d38259873ddec7e9e8c1
vladimir-kuzemchak <vladimirkuzemcak@gmail.com>
2021-05-13 15:33:34 +0300

    first commit

---
+++ 300421/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _300421
+{
+    class Program
+    {
+        //GetMin for five values
+        
+        /*
+         Вызов перегруженых методов с дефолтными значениями не является возможным, так как это не безопасно
+        */
+
+        static void Main(string[] args)
+        {
+
+            int tempResult = GetMin(133,2,44,221,11);
+            Console.Write($"Min value: {tempResult} \n");
+            
+
+        }
+
+        #region First variant (overloading)
+
+        public static int GetMin(int firstNumber, int secondNumber)
+        {
+            return firstNumber > secondNumber ? secondNumber : firstNumber;
+        }
+
+        public static int GetMin(int firstNumber, int secondNumber, int thirdNumber)
+        {
+            int tmpResult = GetMin(firstNumber, secondNumber);
+            return tmpResult < thirdNumber ? tmpResult : thirdNumber;
+        }
+
+        public static int GetMin(int firstNumber, int secondNumber, int thirdNumber, int fourthNumber)
+        {
+            int tmpResult = GetMin(firstNumber, secondNumber, thirdNumber);
+            return tmpResult < fourthNumber ? tmpResult : fourthNumber;
+        }
+
+        public static int GetMin(int firstNumber, int secondNumber, int thirdNumber, int fourthNumber, int fifthNumber)
+        {
+            int tmpResult = GetMin(firstNumber, secondNumber, thirdNumber, fourthNumber);
+            return tmpResult < fifthNumber ? tmpResult : fifthNumber;
+        }
+
+        #endregion
+
+        #region Second variant (default parametrs)
+
+        //public static int GetMin(int firstNumber,int secondNumber = 2)
+        //{
+        //    int minValue = (
+        //                   (firstNumber > secondNumber ? secondNumber : firstNumber)
+        //                   );
+
+        //    return minValue;
+        //}
+
+        //public static int GetMin(int firstNumber, int secondNumber = 2, int thirdNumber = 3)
+        //{
+        //    int minValue = (
+        //                   (thirdNumber >
+        //                   (firstNumber = (firstNumber > secondNumber ? secondNumber : firstNumber))
+        //                   ? firstNumber : thirdNumber)
+        //                   );
+
+        //    return minValue;
+        //}
+
+        //public static int GetMin(int firstNumber, int secondNumber = 2, int thirdNumber = 3, int fourthNumber = 4)
+        //{
+        //    int minValue = (
+        //                   ((fourthNumber > (secondNumber =
+        //                   (thirdNumber > (firstNumber =
+        //                   (firstNumber > secondNumber ? secondNumber : firstNumber)
+        //                   ) ? firstNumber : thirdNumber)
+        //                   ) ? secondNumber : fourthNumber))
+        //                   );
+
+        //    return minValue;
+        //}
+
+        //public static int GetMin(int firstNumber, int secondNumber = 2, int thirdNumber = 3, int fourthNumber = 4, int fifthNumber = 5)
+        //{
+        //    int minValue = (
+        //                   (fifthNumber > (thirdNumber =
+        //                   (fourthNumber > (secondNumber =
+        //                   (thirdNumber > (firstNumber =
+        //                   (firstNumber > secondNumber ? secondNumber : firstNumber)
+        //                   ) ? firstNumber : thirdNumber)
+        //                   ) ? secondNumber : fourthNumber)
+        //                   ) ? thirdNumber : fifthNumber)
+        //                   );
+
+        //    return minValue;
+        //}
+
+        #endregion
+    }
+}
---
+++ 300421Task2/Program.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _300421Task2
+{
+    class Program
+    {
+        //GetSum for ten values
+
+        static void Main(string[] args)
+        {
+
+            int tempResult = GetSum(1,1,1,1,1,1,1,1,1,1); 
+
+
+            Console.WriteLine($"Sum of values: {tempResult} \n");
+
+        }
+
+        #region first variant
+
+        //public static int GetSum(int firstNumber, int secondNumber)
+        //{
+        //    return firstNumber + secondNumber;
+        //}
+
+        //public static int GetSum(int firstNumber, int secondNumber, int thirdNumber)
+        //{
+        //    return GetSum(GetSum(firstNumber, secondNumber), thirdNumber);
+        //}
+
+        //public static int GetSum(int firstNumber, int secondNumber, int thirdNumber, int fourthNumber)
+        //{
+        //    return GetSum(GetSum(firstNumber, secondNumber, thirdNumber), fourthNumber);
+        //}
+
+        //public static int GetSum(int firstNumber, int secondNumber, int thirdNumber,
+        //                         int fourthNumber, int fifthNumber)
+        //{
+        //    return GetSum(GetSum(firstNumber, secondNumber, thirdNumber, fourthNumber), fifthNumber);
+        //}
+
+        //public static int GetSum(int firstNumber, int secondNumber, int thirdNumber,
+        //                         int fourthNumber, int fifthNumber, int sixthNumber)
+        //{
+        //    return GetSum(GetSum(firstNumber, secondNumber, thirdNumber, fourthNumber, fifthNumber),sixthNumber);
+        //}
+
+        //public static int GetSum(int firstNumber, int secondNumber, int thirdNumber,
+        //                         int fourthNumber, int fifthNumber, int sixthNumber,
+        //                         int seventhNumber)
+        //{
+        //    return GetSum(GetSum(firstNumber, secondNumber, thirdNumber,
+        //                         fourthNumber, fifthNumber, sixthNumber),seventhNumber);
+        //}
+
+
+        //public static int GetSum(int firstNumber, int secondNumber, int thirdNumber,
+        //                         int fourthNumber, int fifthNumber, int sixthNumber,
+        //                         int seventhNumber, int eighthNumber)
+        //{
+        //    return GetSum(GetSum(firstNumber, secondNumber, thirdNumber,
+        //                         fourthNumber, fifthNumber, sixthNumber, seventhNumber),eighthNumber);
+        //}
+
+        //public static int GetSum(int firstNumber, int secondNumber, int thirdNumber,
+        //                         int fourthNumber, int fifthNumber, int sixthNumber,
+        //                         int seventhNumber, int eighthNumber, int ninthNumber)
+        //{
+        //    return GetSum(GetSum(firstNumber, secondNumber, thirdNumber,
+        //                         fourthNumber, fifthNumber, sixthNumber,
+        //                         seventhNumber, eighthNumber),ninthNumber);
+        //}
+
+        //public static int GetSum(int firstNumber, int secondNumber, int thirdNumber,
+        //                         int fourthNumber, int fifthNumber, int sixthNumber,
+        //                         int seventhNumber, int eighthNumber, int ninthNumber,int tenthNumber)
+        //{
+        //    return GetSum(GetSum(firstNumber, secondNumber, thirdNumber,
+        //                         fourthNumber, fifthNumber, sixthNumber,
+        //                         seventhNumber, eighthNumber, ninthNumber),tenthNumber);
+        //}
+
+        #endregion
+
+        #region second variant
+
+        public static int GetSum(int firstNumber, int secondNumber) => firstNumber + secondNumber;
+
+
+        public static int GetSum(int firstNumber, int secondNumber, int thirdNumber) =>
+
+                          GetSum(GetSum(firstNumber, secondNumber), thirdNumber);
+
+
+        public static int GetSum(int firstNumber, int secondNumber,
+                                 int thirdNumber, int fourthNumber) =>
+
+                          GetSum(firstNumber, secondNumber) +
+                          GetSum(thirdNumber, fourthNumber);
+
+
+        public static int GetSum(int firstNumber, int secondNumber,
+                                 int thirdNumber, int fourthNumber, int fifthNumber) =>
+
+                          GetSum(GetSum(firstNumber, secondNumber, thirdNumber, fourthNumber), fifthNumber);
+
+
+        public static int GetSum(int firstNumber, int secondNumber,
+                                 int thirdNumber, int fourthNumber,
+                                 int fifthNumber, int sixthNumber) =>
+
+                          GetSum(firstNumber, secondNumber) +
+                          GetSum(thirdNumber, fourthNumber) +
+                          GetSum(fifthNumber, sixthNumber);
+
+
+        public static int GetSum(int firstNumber, int secondNumber,
+                                 int thirdNumber, int fourthNumber,
+                                 int fifthNumber, int sixthNumber, int seventhNumber) =>
+
+                          GetSum(GetSum(firstNumber, secondNumber, thirdNumber,
+                                        fourthNumber, fifthNumber, sixthNumber), seventhNumber);
+
+
+        public static int GetSum(int firstNumber, int secondNumber,
+                                 int thirdNumber, int fourthNumber,
+                                 int fifthNumber, int sixthNumber,
+                                 int seventhNumber, int eighthNumber) =>
+
+                          GetSum(firstNumber, secondNumber) +
+                          GetSum(thirdNumber, fourthNumber) +
+                          GetSum(fifthNumber, sixthNumber) +
+                          GetSum(seventhNumber, eighthNumber);
+
+
+
+        public static int GetSum(int firstNumber, int secondNumber,
+                                 int thirdNumber, int fourthNumber,
+                                 int fifthNumber, int sixthNumber,
+                                 int seventhNumber, int eighthNumber, int ninthNumber) =>
+
+                          GetSum(GetSum(firstNumber, secondNumber, thirdNumber,
+                                        fourthNumber, fifthNumber, sixthNumber,
+                                        seventhNumber, eighthNumber), ninthNumber);
+
+
+        public static int GetSum(int firstNumber, int secondNumber,
+                                 int thirdNumber, int fourthNumber,
+                                 int fifthNumber, int sixthNumber,
+                                 int seventhNumber, int eighthNumber,
+                                 int ninthNumber, int tenthNumber) =>
+
+                          GetSum(firstNumber, secondNumber) +
+                          GetSum(thirdNumber, fourthNumber) +
+                          GetSum(fifthNumber, sixthNumber) +
+                          GetSum(seventhNumber, eighthNumber) +
+                          GetSum(ninthNumber, tenthNumber);
+
+        #endregion
+
+    }
+}
